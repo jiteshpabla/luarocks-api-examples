8838ac5d307e130483f1a8db02b77783bf3d98e8
Jitesh Pabla <jiteshpabla97@gmail.com>
2018-08-11 12:11:41 +0530

    Update example for listing rock trees.

--- list_rock_trees_example.lua
+++ list_rock_trees_example.lua
@@ -1,9 +1,15 @@
 local lr = require("luarocks")
 
+rt = lr.list_trees()
+
+-- simple printing
+for _,data_table in ipairs(rt) do
+	print(data_table["name"])
+	print(data_table["root"])
+	print()
+end
+
 -- this is best viewed as a structured table
 -- hence, it requires you to install the penlight rock
-local pl = require("pl.pretty")
-
-local rt = lr.list_rock_trees()
-
+pl = require("pl.pretty")
 print(pl.write(rt))
