b8ec57398b3d7b62a57ac5d6c1e9ecaca0527424
Jitesh Pabla <jiteshpabla97@gmail.com>
2018-08-11 12:14:41 +0530

    Update list_example according to API changes.

--- list_example.lua
+++ list_example.lua
@@ -1,11 +1,17 @@
 local lr = require("luarocks.init")
 
---- List API-function call and print
-list_of_rocks = lr.list()
-for i, data_table in ipairs(list_of_rocks) do
-	print(i, data_table)
-	for _, data in ipairs(data_table) do
-		print(data)
+-- this is best viewed as a structured table
+-- hence, it requires you to install the penlight rock
+pl = require("pl.pretty")
+print(pl.write(list_of_rocks.date))
+
+-- for viewing with for loops
+for rock_name, data_table in pairs(list_of_rocks) do
+	for version, location_table in pairs(data_table) do
+		for _, location in pairs(location_table) do
+			print(rock_name)
+			print(version)
+			print(location.repo)
+		end
 	end
-	print("\n")
 end
